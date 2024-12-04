5fc45adc623e2bffd9167e8605f52e3eba37f2cc
RAFIFEFZET <mrafiffawwaz040706@gmail.com>
2024-12-04 17:03:46 +0700

    Remove merge conflict markers and format keywords in composer.json

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-<<<<<<< HEAD
     App\Providers\Filament\AdminPanelProvider::class,
-=======
->>>>>>> 5782c6619d95732890caffc52412e0e864d96933
 ];
