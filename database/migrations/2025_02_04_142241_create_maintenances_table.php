ead26946a3d959e5a497726a53ca9a40113ef860
JexelCodez <jjzefanya6002@gmail.com>
2025-02-04 21:38:51 +0700

    Menambahkan table periode dan maintenance serta menghapus table yang tidak perlu

---
+++ database/migrations/2025_02_04_142241_create_maintenances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintenances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_periode_pemeliharaan');
+            $table->unsignedBigInteger('id_user');
+            $table->text('deskripsi_tugas');
+            $table->enum('status', ['sedang diproses', 'dalam perbaikan', 'selesai']);
+            $table->date('tanggal_pelaksanaan');
+            $table->foreign('id_periode_pemeliharaan')->references('id')->on('periode_pemeliharaans');
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintenances');
+    }
+};
