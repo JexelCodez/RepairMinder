7e01962f62e03223ec095f9b8176c86bb40114e0
RAFIFEFZET <mrafiffawwaz040706@gmail.com>
2025-02-04 13:53:01 +0700

    nyamain tema admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,9 +34,10 @@ class AdminPanelProvider extends PanelProvider
             ->passwordReset()
             ->sidebarCollapsibleOnDesktop()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#00bcd5',
             ])
             ->brandLogo(asset('images/logo-sija.png'))
+            ->favicon(asset('icons/remi-icon.png'))
             ->brandLogoHeight('5rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
