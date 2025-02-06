942aa30eafcdd5d1605650ef6627f0c5160967ac
RAFIFEFZET <mrafiffawwaz040706@gmail.com>
2025-02-06 16:19:42 +0700

    update form login regist tipis tipis

--- resources/views/user_view/auth/login.blade.php
+++ resources/views/user_view/auth/login.blade.php
@@ -13,6 +13,16 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
     <style>
+        :root {
+            --tg-color-primary: linear-gradient(180deg, #2abdbb 4%, #00aeef 98.89%);
+            --tg-color-section-body: linear-gradient(
+                180deg,
+                #2d2b6f 0%,
+                #066a8a 128.75%
+            );
+            --tg-color-white-default: #ffffff;
+            --tg-theme-primary: #3083dc;
+        }
         .input-icon {
             position: absolute;
             top: 50%;
@@ -25,6 +35,115 @@
     
         .position-relative .form-control {
             padding-right: 2.5rem; /* Memberi ruang untuk ikon */
+        }
+        .btn {
+            user-select: none;
+            -moz-user-select: none;
+            background: var(--tg-color-primary);
+            background-image: url("../images/templatemo-wave-header.jpg"),
+                linear-gradient(#348cd2, #ffffff);
+            border: medium none;
+            -webkit-border-radius: 10px;
+            -moz-border-radius: 10px;
+            -o-border-radius: 10px;
+            -ms-border-radius: 10px;
+            border-radius: 10px;
+            color: var(--tg-color-white-default);
+            cursor: pointer;
+            display: inline-flex;
+            align-items: center;
+            font-size: 14px;
+            font-weight: 700;
+            letter-spacing: 0;
+            line-height: 1;
+            margin-bottom: 0;
+            padding: 18px 24px;
+            text-align: center;
+            text-transform: uppercase;
+            touch-action: manipulation;
+            -webkit-transition: all 0.3s ease-out 0s;
+            -moz-transition: all 0.3s ease-out 0s;
+            -ms-transition: all 0.3s ease-out 0s;
+            -o-transition: all 0.3s ease-out 0s;
+            transition: all 0.3s ease-out 0s;
+            vertical-align: middle;
+            white-space: nowrap;
+            position: relative;
+            overflow: hidden;
+            z-index: 1;
+        }
+        .btn::before {
+            content: "";
+            position: absolute;
+            -webkit-transition-duration: 800ms;
+            transition-duration: 800ms;
+            width: 200%;
+            height: 200%;
+            top: 110%;
+            left: 50%;
+            background: var(--tg-color-section-body);
+            -webkit-transform: translateX(-50%);
+            -moz-transform: translateX(-50%);
+            -ms-transform: translateX(-50%);
+            -o-transform: translateX(-50%);
+            transform: translateX(-50%);
+            -webkit-border-radius: 50%;
+            -moz-border-radius: 50%;
+            -o-border-radius: 50%;
+            -ms-border-radius: 50%;
+            border-radius: 50%;
+            z-index: -1;
+        }
+        .btn::after {
+            font-weight: 900;
+            margin-left: 8px;
+            font-size: 20px;
+            line-height: 0;
+            transition: all 0.3s ease-out 0s;
+        }
+        .btn:hover,
+        .btn:focus-visible {
+            color: var(--tg-color-white-default);
+            /* background: var(--tg-theme-primary); */
+        }
+        .btn:hover:before,
+        .btn:focus-visible:before {
+            top: -40%;
+        }
+        .btn.btn-login {
+            width: 100%;
+            border-radius: 11px;
+            /* padding: 20px 26px; */
+            justify-content: center;
+            font-size: 18px;
+        }
+        .btn.btn-login:hover {
+            background-color: var(--tg-theme-secondary);
+        }
+        a{
+            color: var(--tg-theme-primary);
+            outline: none;
+            text-decoration: none;
+            -webkit-transition: all 0.3s ease-out 0s;
+            -moz-transition: all 0.3s ease-out 0s;
+            -ms-transition: all 0.3s ease-out 0s;
+            -o-transition: all 0.3s ease-out 0s;
+            transition: all 0.3s ease-out 0s;
+        }
+
+        a:focus {
+            text-decoration: none;
+            outline: none;
+            -webkit-box-shadow: none;
+            -moz-box-shadow: none;
+            -ms-box-shadow: none;
+            -o-box-shadow: none;
+            box-shadow: none;
+        }
+
+        a:hover {
+            color: var(--tg-theme-primary);
+            text-decoration: none;
         }
     </style>
     
@@ -64,10 +183,10 @@
                             </div>
                             
                             <!-- Submit Button -->
-                            <button type="submit" class="btn btn-primary w-100 mb-3">Login</button>
+                            <button type="submit" class="btn btn-login mb-3">Login</button>
                             <!-- Forgot Password Link -->
                             <div class="mt-2 text-end">
-                                <a href="{{ route('password.request') }}" class="text-decoration-none small text-muted">Lupa Password?</a>
+                                <a href="{{ route('password.request') }}" class="text-decoration-none small">Lupa Password?</a>
                             </div>
                         </form>
                         <!-- Register Link -->
