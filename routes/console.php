c8fd8389fd6d829a67c5a5261faae12039d0df05
iqbal <thesecretiqbal@gmail.com>
2025-02-14 13:53:01 +0700

    command

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,11 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use App\Jobs\SyncBarangJob;
+use Illuminate\Support\Facades\Schedule;
 
 // Command untuk men-schedule job sync barang
 Artisan::command('sync:barang', function () {
     SyncBarangJob::dispatch();
 })->purpose('Sinkronisasi barang dari API A ke aplikasi B');
+
+Schedule::command('maintenance:reminder')->everyMinute();
